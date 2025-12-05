e94bd6102b87b86f19fa7ce8e57d111694517887
Alessandrini Antoine <a.alsdrini@gmail.com>
2025-12-05 17:05:33 +0100

    sofa example gui

---
+++ examples/gui/sofa/model.py
@@ -0,0 +1,43 @@
+import numpy as np
+# Auto imports
+from pySimBlocks.blocks.operators import DiscreteIntegrator
+from pySimBlocks.blocks.operators import Gain
+from pySimBlocks.blocks.operators import Sum
+from pySimBlocks.blocks.sources import Step
+from pySimBlocks.blocks.systems import SofaSystem
+from pySimBlocks import Model, Simulator
+from parameters_auto import *
+
+model = Model('auto_model')
+
+step = Step('step', value_before=step_value_before, value_after=step_value_after, t_step=step_t_step)
+model.add_block(step)
+
+error = Sum('error', num_inputs=error_num_inputs, signs=error_signs)
+model.add_block(error)
+
+Kp = Gain('Kp', gain=Kp_gain)
+model.add_block(Kp)
+
+Ki = Gain('Ki', gain=Ki_gain)
+model.add_block(Ki)
+
+discrete_integrator = DiscreteIntegrator('discrete_integrator', initial_state=discrete_integrator_initial_state)
+model.add_block(discrete_integrator)
+
+sum = Sum('sum', num_inputs=sum_num_inputs)
+model.add_block(sum)
+
+sofa = SofaSystem('sofa', scene_file=sofa_scene_file, input_keys=sofa_input_keys, output_keys=sofa_output_keys)
+model.add_block(sofa)
+
+model.connect('step', 'out', 'error', 'in1')
+model.connect('error', 'out', 'Kp', 'in')
+model.connect('error', 'out', 'discrete_integrator', 'in')
+model.connect('Kp', 'out', 'sum', 'in1')
+model.connect('discrete_integrator', 'out', 'Ki', 'in')
+model.connect('Ki', 'out', 'sum', 'in2')
+model.connect('sum', 'out', 'sofa', 'cable')
+model.connect('sofa', 'measure', 'error', 'in2')
+
+sim = Simulator(model, dt=dt)
---
+++ examples/gui/sofa/run.py
@@ -0,0 +1,38 @@
+from model import sim, T, dt
+import numpy as np
+import matplotlib.pyplot as plt
+
+logs = sim.run(T=T, variables_to_log=[
+    'step.outputs.out',
+    'sofa.outputs.measure',
+    'sum.outputs.out',
+])
+print('Simulation complete.')
+
+length = len(next(iter(logs.values())))
+time = np.arange(0, T, dt)[:length]
+
+plt.figure()
+step_outputs_out = np.array(logs['step.outputs.out']).reshape(length, -1)
+for i in range(step_outputs_out.shape[1]):
+    plt.step(time, step_outputs_out[:, i], where='post', label='step_outputs_out'+str(i))
+sofa_outputs_measure = np.array(logs['sofa.outputs.measure']).reshape(length, -1)
+for i in range(sofa_outputs_measure.shape[1]):
+    plt.step(time, sofa_outputs_measure[:, i], where='post', label='sofa_outputs_measure'+str(i))
+plt.xlabel('Time [s]')
+plt.ylabel('Values')
+plt.title('Outputs')
+plt.legend()
+plt.grid()
+
+plt.figure()
+sum_outputs_out = np.array(logs['sum.outputs.out']).reshape(length, -1)
+for i in range(sum_outputs_out.shape[1]):
+    plt.step(time, sum_outputs_out[:, i], where='post', label='sum_outputs_out'+str(i))
+plt.xlabel('Time [s]')
+plt.ylabel('Values')
+plt.title('Command')
+plt.legend()
+plt.grid()
+
+plt.show()
