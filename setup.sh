cad807c6fb7e03afb9d7699d917d103ee1f5505a
Alessandrini Antoine <a.alsdrini@gmail.com>
2026-02-12 14:54:58 +0100

    [remove] setup script

--- setup.sh
+++
@@ -1,9 +0,0 @@
-
-# Source _env/bin/activate if it exists
-if [ -f "_env/bin/activate" ]; then
-    source _env/bin/activate
-fi
-# Export PYTHONPATH to include the current directory
-export PYTHONPATH=$(pwd):$PYTHONPATH
-# Print the PYTHONPATH for verification
-echo "PYTHONPATH set to: $PYTHONPATH"
