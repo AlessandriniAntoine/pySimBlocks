da55dda353d95af11591a6ac22e71f2d2f71b4a0
Alessandrini Antoine <a.alsdrini@gmail.com>
2026-02-16 18:53:46 +0100

    feature(gui): move dialog display to meta

--- pySimBlocks/gui/blocks/block_meta.py
+++ pySimBlocks/gui/blocks/block_meta.py
@@ -22,6 +22,19 @@ from abc import ABC
 from pathlib import Path
 from typing import Any, Dict, List, Literal
 
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import (
+    QComboBox,
+    QFormLayout,
+    QFrame,
+    QLabel,
+    QLineEdit,
+    QSizePolicy,
+    QTextBrowser,
+    QVBoxLayout,
+    QWidget,
+)
+
 from pySimBlocks.gui.blocks.parameter_meta import ParameterMeta
 from pySimBlocks.gui.blocks.port_meta import PortMeta
 from pySimBlocks.gui.model import BlockInstance, PortInstance
@@ -86,6 +99,13 @@ class MyBlockMeta(BlockMeta):
     inputs: List[PortMeta] = []
     outputs: List[PortMeta] = []
 
+    param_widgets: Dict[str, QWidget] = {}
+    param_labels: Dict[str, QLabel] = {}
+
+
+    # --------------------------------------------------------------------------
+    # Parameter resolution
+    # --------------------------------------------------------------------------
     def get_param(self, param_name: str) -> ParameterMeta | None:
         for param in self.parameters:
             if param.name == param_name:
@@ -98,7 +118,11 @@ class MyBlockMeta(BlockMeta):
         Children override if needed.
         """
         return True
+
     
+    # --------------------------------------------------------------------------
+    # Port resolution
+    # --------------------------------------------------------------------------
     def resolve_port_group(self, 
                            port_meta: PortMeta,
                            direction: Literal['input', 'output'], 
@@ -110,6 +134,7 @@ class MyBlockMeta(BlockMeta):
         """
         return [PortInstance(port_meta.name, port_meta.display_as, direction, instance)]
     
+    # ------------------------------------------------------------
     def build_ports(self, instance: "BlockInstance") -> list["PortInstance"]:
         ports = []
 
@@ -120,3 +145,164 @@ class MyBlockMeta(BlockMeta):
             ports.extend(self.resolve_port_group(pmeta, "output", instance))
 
         return ports
+
+
+    # --------------------------------------------------------------------------
+    # QT dialog display 
+    # --------------------------------------------------------------------------
+    def build_description(self, instance: BlockInstance, form: QFormLayout):
+        """ Default description display. Children can override if needed. """
+        title = QLabel(f"<b>{instance.meta.name}</b>")
+        title.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        form.addRow(title)
+
+        frame = QFrame()
+        frame.setFrameShape(QFrame.StyledPanel)
+        frame.setFrameShadow(QFrame.Raised)
+        frame.setLineWidth(1)
+
+        frame_layout = QVBoxLayout(frame)
+        frame_layout.setContentsMargins(8, 6, 8, 6)
+
+        desc = QTextBrowser()
+        desc.setMarkdown(instance.meta.description)
+        desc.setReadOnly(True)
+        desc.setFrameShape(QFrame.NoFrame)
+        desc.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Minimum)
+        desc.document().setTextWidth(400)
+        desc.setFixedHeight(int(desc.document().size().height()) + 6)
+
+        frame_layout.addWidget(desc)
+        form.addRow(frame)
+
+
+    # ------------------------------------------------------
+    def build_pre_param(self, 
+                        instance: BlockInstance,
+                        form: QFormLayout, 
+                        readonly: bool = False):
+        """ Default: no pre-parameter widgets. Children override if needed. """
+        pass
+
+    # ------------------------------------------------------------
+    def build_param(self, 
+                    instance: BlockInstance,
+                    form: QFormLayout, 
+                    readonly: bool = False):
+        """ Default: no parameter widgets. Children override if needed. """
+
+
+        # --- Block name ---
+        self.name_edit = QLineEdit(instance.name)
+        form.addRow(QLabel("Block name:"), self.name_edit)
+        if readonly:
+            self.name_edit.setReadOnly(True)
+
+        for param_meta in self.parameters:
+            param_name = param_meta.name
+
+            widget = self._create_param_widget(param_meta, 
+                                               instance.parameters, readonly)
+            if widget is None:
+                continue
+            if readonly:
+                if isinstance(widget, QLineEdit):
+                    widget.setReadOnly(True)
+                    widget.setStyleSheet("""
+                        QLineEdit {
+                            background-color: #2b2b2b;
+                            color: #888888;
+                            border: 1px solid #444444;
+                        }
+                    """)
+                elif isinstance(widget, QComboBox):
+                    widget.setEnabled(False)
+
+            label = QLabel(f"{param_name}:")
+            if param_meta.description:
+                label.setToolTip(param_meta.description)
+            form.addRow(label, widget)
+
+            self.param_widgets[param_name] = widget
+            self.param_labels[param_name] = label
+
+
+    # ------------------------------------------------------------
+    def build_post_param(self, 
+                         instance: BlockInstance,
+                         form: QFormLayout, 
+                         readonly: bool = False):
+        """ Default: no post-parameter widgets. Children override if needed. """
+        pass
+
+    # ------------------------------------------------------------
+    def refresh_form(self, params: Dict[str, Any]):
+        """
+        Refresh the parameter widgets visibility based on
+        BlockMeta.is_parameter_active and current local_params.
+        """
+
+        for param_name, widget in self.param_widgets.items():
+            label = self.param_labels[param_name]
+
+            active = self.is_parameter_active(param_name, params)
+
+            widget.setVisible(active)
+            label.setVisible(active)
+
+
+    # --------------------------------------------------------------------------
+    # Private methods
+    # --------------------------------------------------------------------------
+    def _create_param_widget(self, 
+                             meta: ParameterMeta, 
+                             inst_params: dict[str, Any],
+                             readonly: bool = False
+                             ) -> QWidget:
+        # ENUM
+        if meta.type == "enum":
+            return self._create_enum_widget(meta, inst_params, readonly)
+        
+        # Default: text edit
+        return self._create_edit_widget(meta, inst_params, readonly)
+
+
+    # ------------------------------------------------------------
+    def _create_edit_widget(self,
+                            meta: ParameterMeta,
+                            inst_params: dict[str, Any],
+                            readonly: bool = False) -> QLineEdit:
+        edit = QLineEdit()
+        value = inst_params.get(meta.name)
+        if value is not None:
+            edit.setText(str(value))
+        elif meta.default:
+            edit.setText(str(meta.default))
+        edit.textChanged.connect(
+            lambda val: self._on_param_changed(val, meta.name, inst_params, readonly)
+        )
+        return edit
+
+    # ------------------------------------------------------------
+    def _create_enum_widget(self,
+                            meta: ParameterMeta,
+                            inst_params: dict[str, Any],
+                            readonly: bool = False) -> QComboBox:
+        combo = QComboBox()
+        for v in meta.enum:
+            combo.addItem(str(v))
+        value = inst_params.get(meta.name)
+        if value is not None:
+            combo.setCurrentText(str(value))
+        combo.currentTextChanged.connect(
+            lambda val: self._on_param_changed(val, meta.name, inst_params, readonly)
+        )
+        return combo
+
+
+    # ------------------------------------------------------------
+    def _on_param_changed(self, value, name, params, readonly):
+        if readonly:
+            return
+        params[name] = value
+        self.refresh_form(params)
