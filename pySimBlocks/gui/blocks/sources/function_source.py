28f29f0c8f427b2fb254613d37319b44a8168356
Alessandrini Antoine <a.alsdrini@gmail.com>
2026-02-19 02:20:32 +0100

    fix(header): add missing header

--- pySimBlocks/gui/blocks/sources/function_source.py
+++ pySimBlocks/gui/blocks/sources/function_source.py
@@ -1,4 +1,7 @@
 # ******************************************************************************
+#                                  pySimBlocks
+#                     Copyright (c) 2026 Université de Lille & INRIA
+# ******************************************************************************
 #  This program is free software: you can redistribute it and/or modify it
 #  under the terms of the GNU Lesser General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or (at your
